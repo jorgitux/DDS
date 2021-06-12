1a8e1e6a6fb337d980d7bc79a72f99af74f75a03
jorgitux <yeshuahamashiajg@gmail.com>
2021-06-11 21:46:30 -0500

    Mértricas: % vulnerabilidades por criticidad → Q1 % vulnerabilidades por tecnología (lenguaje) → Q1

--- Trabajo.R
+++ Trabajo.R
@@ -2,15 +2,15 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(forcats)
+#library(forcats)
 
 # App1
-app1_report <- read.csv("data-csv/App1.csv",header = TRUE, sep = ";",)
+app1_report <- read.csv("data-csv/App1 - 2021.csv",header = TRUE, sep = ";")
 # App2
-app2_report <- read.csv("data-csv/App2.csv",header = TRUE, sep = ";")
+app2_report <- read.csv("data-csv/App2 - 2021.csv",header = TRUE, sep = ";")
 
 # Union de reporte
-app_report <- rbind(app1_report, slice(app2_report, 2:nrow(app2_report)) )
+app_report <- rbind(app1_report, app2_report)
 
 # Convierte datos necesarios a datos categóricos
 app_report$Application <- as.factor(app_report$Application)
@@ -22,17 +22,29 @@ app_report$Impact <- as.factor(app_report$Impact)
 app_report$Severity <- as.factor(app_report$Severity)
 app_report$Kingdom <- as.factor(app_report$Kingdom)
 
+# Convierte datos necesarios a datos de tipo Date
+app_report$Found.Date <- as.Date(app_report$Found.Date)
+app_report$Removed.Date <- as.Date(app_report$Removed.Date)
+
+# Convierte datos necesarios a datos de tipo Lógico
+app_report$Is.Reviewed <- as.logical(app_report$Is.Reviewed)
+app_report$Has.Correlated.Issues <- as.logical(app_report$Has.Correlated.Issues)
+app_report$Has.Comments <- as.logical(app_report$Has.Comments)
+app_report$Is.Removed <- as.logical(app_report$Is.Removed)
+app_report$Is.Suppressed <- as.logical(app_report$Is.Suppressed)
+app_report$Is.Hidden <- as.logical(app_report$Is.Hidden)
+
 #Transformar CWEs a vector (Recordar que hay blancos)
 #x<-list(app_report$CWE)
 
 # Variables Generales
-num_total_vulnerabilities = nrow(app_report)
+num_vulns_actually = nrow(app_report)
 
 # Agrupación por Criticidad
 df <- app_report %>% group_by(Criticality) %>% summarise(num = n())
 
 
-df[,"Percent"] <- df[,"num"] / num_total_vulnerabilities*100
+df[,"Percent"] <- df[,"num"] / num_vulns_actually*100
 
 
 df %>%
@@ -45,7 +57,7 @@ df %>%
 
 # Agrupación por Lenguaje
 df_lang <- app_report %>% group_by(Language) %>% summarise(num = n())
-df_lang[,"Percent"] <- df_lang[,"num"] / num_total_vulnerabilities*100
+df_lang[,"Percent"] <- df_lang[,"num"] / num_vulns_actually*100
 
 df_lang %>%
   mutate(name = fct_reorder(Language, Percent)) %>%
@@ -54,3 +66,10 @@ df_lang %>%
   coord_flip() +
   xlab("Language") +
   theme_bw()
+
+
+# Agrupación por Aplicación y Criticidad
+df_app_criticality <- app_report %>% group_by(Application,Criticality) %>% summarise(num = n())
+
+# Agrupación por Aplicación y Vulnerabilidad
+df_app_vuln <- app_report %>% group_by(Application,CWE) %>% summarise(num = n())
