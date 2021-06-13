8fab6a5a7bf15f3cdc57a7ab3ead09b9cb2765bb
jorgitux <yeshuahamashiajg@gmail.com>
2021-06-12 21:08:50 -0500

    - Actualización de métricas - Actualización de Markdown

--- Trabajo.R
+++ Trabajo.R
@@ -2,7 +2,7 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-#library(forcats)
+library(forcats)
 
 # App1
 app1_report <- read.csv("data-csv/App1 - 2021.csv",header = TRUE, sep = ";")
@@ -23,8 +23,8 @@ app_report$Severity <- as.factor(app_report$Severity)
 app_report$Kingdom <- as.factor(app_report$Kingdom)
 
 # Convierte datos necesarios a datos de tipo Date
-app_report$Found.Date <- as.Date(app_report$Found.Date)
-app_report$Removed.Date <- as.Date(app_report$Removed.Date)
+app_report$Found.Date <- as.Date(app_report$Found.Date,"%d/%m/%Y")
+app_report$Removed.Date <- as.Date(app_report$Removed.Date,"%d/%m/%Y")
 
 # Convierte datos necesarios a datos de tipo Lógico
 app_report$Is.Reviewed <- as.logical(app_report$Is.Reviewed)
@@ -34,25 +34,22 @@ app_report$Is.Removed <- as.logical(app_report$Is.Removed)
 app_report$Is.Suppressed <- as.logical(app_report$Is.Suppressed)
 app_report$Is.Hidden <- as.logical(app_report$Is.Hidden)
 
-#Transformar CWEs a vector (Recordar que hay blancos)
-#x<-list(app_report$CWE)
-
 # Variables Generales
 num_vulns_actually = nrow(app_report)
 
-# Agrupación por Criticidad
-df <- app_report %>% group_by(Criticality) %>% summarise(num = n())
+# Agrupación por Severidad
+df <- app_report %>% group_by(Severity) %>% summarise(num = n())
 
 
 df[,"Percent"] <- df[,"num"] / num_vulns_actually*100
 
 
 df %>%
-  mutate(name = fct_reorder(Criticality, Percent)) %>%
-  ggplot( aes(x=Criticality, y=Percent)) +
+  mutate(name = fct_reorder(Severity, Percent)) %>%
+  ggplot( aes(x=Severity, y=Percent)) +
   geom_bar(stat="identity", fill="red", alpha=.6, width=.4) +
   coord_flip() +
-  xlab("Criticality") +
+  xlab("Severity") +
   theme_bw()
 
 # Agrupación por Lenguaje
@@ -68,8 +65,25 @@ df_lang %>%
   theme_bw()
 
 
-# Agrupación por Aplicación y Criticidad
-df_app_criticality <- app_report %>% group_by(Application,Criticality) %>% summarise(num = n())
+# Agrupación por Aplicación y Severidad
+df_app_severity <- app_report %>% group_by(Application,Severity) %>% summarise(num = n())
 
-# Agrupación por Aplicación y Vulnerabilidad
-df_app_vuln <- app_report %>% group_by(Application,CWE) %>% summarise(num = n())
+
+df_app_severity %>% ggplot( aes(fill=Severity, y=num, x=Application) ) +
+                      geom_bar(position = "dodge", stat="identity") +
+                      ylab("Nº Vulns")
+
+# Agrupación de vulnerabilidades por Aplicación
+df_app <- app_report %>% group_by(Application) %>% summarise(num = n())
+
+
+x <- df_app_severity %>% filter(Severity==5)
+x[,"Percent"] <- x[,"num"]/df_app[,"num"]*100
+
+x %>%
+  mutate(name = fct_reorder(Application, Percent)) %>%
+  ggplot( aes(x=Application, y=Percent)) +
+  geom_bar(stat="identity", fill="blue", alpha=.6, width=.4) +
+  coord_flip() +
+  xlab("Application") +
+  theme_bw()
